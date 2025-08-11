21322ced694d1f04bcdacbc8033601aff8561e88
Babak Naimi <naimi.b@gmail.com>
2025-08-11 03:51:22 +0200

    edit template and include geotiff codes

--- src/include/geotiff_extension.hpp
+++ src/include/geotiff_extension.hpp
@@ -1,28 +1,14 @@
+#pragma once
+
 #include "duckdb.hpp"
-#include "duckdb/main/extension.hpp"
-#include "geotiff_register.hpp"
 
 namespace duckdb {
 
-class GeotiffExtension final : public Extension {
+class GeotiffExtension : public Extension {
 public:
-  void Load(DuckDB &db) override { RegisterGeoTiff(*db.instance); }
-  std::string Name() override { return "geotiff"; }
-  std::string Version() const override { return DuckDB::LibraryVersion(); }
+	void Load(DuckDB &db) override;
+	std::string Name() override;
+	std::string Version() const override;
 };
 
 } // namespace duckdb
-
-// #pragma once
-// 
-// #include "duckdb.hpp"
-// 
-// namespace duckdb {
-// class GeotiffExtension : public Extension {
-// public:
-// 	void Load(DuckDB &db) override;
-// 	std::string Name() override;
-// 	std::string Version() const override;
-// };
-// 
-// } // namespace duckdb
